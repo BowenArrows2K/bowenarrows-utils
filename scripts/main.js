dbdcf430468452d11696042f93b5e1f72a63fc9a
BowenArrows2K <bowen.quaid@gmail.com>
2025-03-29 14:46:57 -0500

    Fix Buttons v0.0.1

--- scripts/main.js
+++ scripts/main.js
@@ -24,17 +24,17 @@ function togglePartyList() {
 const showButtons = () => {
     const CoinSpenderButton = document.createElement("li");
     CoinSpenderButton.id = "currency-spender-button";
-    CoinSpenderButton.attributes["data-tool"] = "CurrencySpender";
-    CoinSpenderButton.attributes["data-tooltip"] = "Currency Spender";
-    CoinSpenderButton.attributes["aria-label"] = "Show Currency Spender";
+    CoinSpenderButton.setAttribute("data-tool", "CurrencySpender");
+    CoinSpenderButton.setAttribute("data-tooltip", "Currency Spender");
+    CoinSpenderButton.setAttribute("aria-label", "Show Currency Spender");
     CoinSpenderButton.innerHTML = `<i class="fas fa-coins"></i>`;
     CoinSpenderButton.onclick = () => toggleCurrencySpender();
 
     const PartyListButton = document.createElement("li");
     PartyListButton.id = "party-list-button";
-    PartyListButton.attributes["data-tool"] = "PartyList";
-    PartyListButton.attributes["data-tooltip"] = "Party List";
-    PartyListButton.attributes["aria-label"] = "Show Party List";
+    PartyListButton.setAttribute("data-tool", "PartyList");
+    PartyListButton.setAttribute("data-tooltip", "Party List");
+    PartyListButton.setAttribute("aria-label", "Show Party List");
     PartyListButton.innerHTML = `<i class="fas fa-users"></i>`;
     PartyListButton.onclick = () => togglePartyList();
 
