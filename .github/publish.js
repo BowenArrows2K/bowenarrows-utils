fac68049ad2ea453775d5a76cb9450a3041b09c3
BowenArrows2K <bowen.quaid@gmail.com>
2025-03-30 15:12:04 -0500

    Add versioning to Release Notes

--- .github/publish.js
+++ .github/publish.js
@@ -2,6 +2,7 @@ const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fet
 
 const version = process.env.RELEASE_VERSION; // e.g., "0.0.2"
 const [owner, repo] = process.env.REPO.split('/');
+const changelogBlob = version.split('.').slice(0, 2).join('.'); // e.g., "002"
 
 (async () => {
   const response = await fetch("https://api.foundryvtt.com/_api/packages/release_version/", {
@@ -16,7 +17,7 @@ const [owner, repo] = process.env.REPO.split('/');
       release: {
         version: version,
         manifest: `https://github.com/${owner}/${repo}/releases/download/${version}/module.json`,
-        notes: `https://github.com/${owner}/${repo}/blob/main/CHANGELOG.md`,
+        notes: `https://github.com/${owner}/${repo}/blob/main/CHANGELOG.md#v-${changelogBlob}`,
         compatibility: {
           minimum: "12",
           verified: "12",
